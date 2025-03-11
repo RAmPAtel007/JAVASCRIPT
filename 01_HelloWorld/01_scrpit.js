45c3631b25a2cbe0de7eeb3554059647e3c7b911
RAmPAtel007 <codewizard0007@gmail.com>
2025-03-11 11:44:55 +0530

    update

--- 01_HelloWorld/01_scrpit.js
+++ 01_HelloWorld/01_scrpit.js
@@ -1,2 +1,13 @@
 console.log('hello world!!!')
 console.log(5+10)
+console.log(`this supports new 
+ line and spacing  `)
+// console.log("does not support new line")    
+console.log(+'100') // string to number 
+let a = parseInt('200 sjidjoi');
+console.log(a);
+console.log('100ag') //string
+
+//number to string
+console.log('a'+ 100)
+
