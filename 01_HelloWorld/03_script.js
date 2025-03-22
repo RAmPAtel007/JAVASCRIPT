ddd29056b8ef8df4be6b2239786992175fd1d522
RAmPAtel007 <codewizard0007@gmail.com>
2025-03-22 23:23:04 +0530

    update

---
+++ 01_HelloWorld/03_script.js
@@ -0,0 +1,7 @@
+const width = +prompt("enter the width") // + is for conversion of string to number 
+const height = +prompt ("enter the height ") 
+const areaRec = width * height; 
+console.log(areaRec);
+
+console.log(width * height);
+document.write(`Rectangle Area: ${width * height}`); // to show the js output on the webpage it is a shortcut 
